6c358e0b1899e67f1852c96b3fdab98fffbec7bb
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-03-17 12:45:03 +0100

    practice

--- index.js
+++ index.js
@@ -53,16 +53,90 @@
 // // }); 
 
 
-const form = document.getElementById('name-form');
+// const form = document.getElementById('name-form');
 
-form.addEventListener('submit', consoleForm )
+// form.addEventListener('submit', welcomeFunc)
 
-function consoleForm(event) {
-  event.preventDefault();
-  // console.log(event);
+// function welcomeFunc(event) {
+//   event.preventDefault()
 
-  const form = event.target
-  const value = form[0].value
-  console.dir(form[0]);
-  console.log(value);
-}
+//   const form = event.target
+//   const value = form[0].value
+
+//   alert(value)
+// }
+
+// form.addEventListener('submit', consoleForm )
+
+// function consoleForm(event) {
+//   event.preventDefault();
+//   // console.log(event);
+
+//   const form = event.target
+//   const value = form[0].value
+//   console.dir(form[0]);
+//   console.log(value);
+// }
+// alert('asd')
+
+// const collection = document.getElementsByClassName('paragraph') // верне масивоподібний елемент
+
+// console.log(collection);
+
+// // for(let i = 0; collection.length; i++) {
+// //   collection[i].style.color = 'green'
+// // }
+
+// // for .. of - спеціальний цикл, який перебирає об'єк по ключам
+
+// for(let p of collection){
+//   p.style.color = 'green'
+// }
+
+// const buttons = document.getElementsByTagName('button'); // масивоподібний об'єкт
+// const arr = [...buttons] // створиться масив // prototype array
+
+// console.log(buttons);
+// console.log(arr);
+
+// arr.forEach((item) => {
+//   item.style.color = 'red'
+// });
+// // Array.from(buttons)
+
+// function hello(event) {
+//   console.log(event);
+//   console.log('hello')
+
+//   event.target.removeEventListener('click', hello)
+// }
+
+// for (let btn of buttons) {
+//   btn.addEventListener('click', hello)
+// }
+
+
+// const btn = document.querySelector('button');
+// // console.log(btn);
+
+// const btn2 = document.querySelector('.super-btn');
+// // console.log(btn2);
+
+// const btn3 = document.querySelector('#btn')
+// // console.log(btn3);
+
+// const btns = document.querySelectorAll("button")
+// console.log(btns);
+
+// const paragraphCollection = document.querySelectorAll('.paragraph:nth-child(even)')
+
+// paragraphCollection[0].innerHTML = 'test' // краще НЕ використовувати
+
+// for(let p of paragraphCollection) {
+//   p.style.color = 'red'
+//   p.style.backgroundColor = 'yellow'
+// }
+
+// console.log(paragraphCollection);
+
+
