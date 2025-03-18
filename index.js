dcbce2acde2eb7539377e8236835f0093d588b17
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-03-18 11:30:46 +0100

    Converter

--- index.js
+++ index.js
@@ -18,46 +18,53 @@
 
 */
 
-const CURRENCY = {
-    USD_TO_UA: 37.65,
-    EUR_TO_UA: 40.61,
-}
+// const CURRENCY = {
+//     USD_TO_UA: 37.65,
+//     EUR_TO_UA: 40.61,
+// }
 
-const converterForm = document.querySelector("#currency-converter-form")
+// const converterForm = document.querySelector("#currency-converter-form")
 
-converterForm.addEventListener('submit', convertHandler)
+// converterForm.addEventListener('submit', convertHandler)
 
-function convertHandler(event) {
-    event.preventDefault();
+// function convertHandler(event) {
+//     event.preventDefault();
 
-    const amount = Number(document.querySelector("#amount").value);
-    const currency = document.querySelector('#currency').value
+//     const amount = Number(document.querySelector("#amount").value);
+//     const currency = document.querySelector('#currency').value
 
-    let convertedAmout;
+//     let convertedAmout;
 
-    const {USD_TO_UA: usdCourse, EUR_TO_UA: eurCourse} = CURRENCY
+//     const {USD_TO_UA: usdCourse, EUR_TO_UA: eurCourse} = CURRENCY
 
-    switch(currency) {
-        case 'USD': {
-            convertedAmout = amount * usdCourse;
-            break
-        }
-        case 'EUR': {
-            convertedAmout = amount * eurCourse
-            break
-        }
-        default: throw new Error('Something problem')
-    }
+//     switch(currency) {
+//         case 'USD': {
+//             convertedAmout = amount * usdCourse;
+//             break
+//         }
+//         case 'EUR': {
+//             convertedAmout = amount * eurCourse
+//             break
+//         }
+//         default: throw new Error('Something problem')
+//     }
 
-    // if (currency === 'USD'){
-    //     convertedAmout = amount * usdCourse
-    // } else if (currency === 'EUR'){
-    //     convertedAmout = amount * eurCourse
-    // } else {
-    //     throw new Error('Something problem')
-    // }
-    const resultDiv = document.querySelector('#result')
-    resultDiv.innerHTML = `${amount} ${currency} = ${convertedAmout.toFixed(2)} UA`
-}
+//     // if (currency === 'USD'){
+//     //     convertedAmout = amount * usdCourse
+//     // } else if (currency === 'EUR'){
+//     //     convertedAmout = amount * eurCourse
+//     // } else {
+//     //     throw new Error('Something problem')
+//     // }
+//     const resultDiv = document.querySelector('#result')
+//     resultDiv.innerHTML = `${amount} ${currency} = ${convertedAmout.toFixed(2)} UA`
+// }
 
 
+
+/*
+
+    Маємо div
+    Маємо 2 кнопки: зробити червоним і зробити зеленим
+    за натисненням кнопки на фоновий колір div має змінитися за div
+*/
